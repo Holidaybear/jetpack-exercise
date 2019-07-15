3ef616745720242999c35504c674a7bff2eb9a8e
Kami <yichen@mozilla.com>
2019-07-15 14:44:59 +0800

    Implement API test

---
+++ app/src/test/java/tw/holidaybear/jetpack/exercise/GitHubAPITest.kt
@@ -0,0 +1,50 @@
+package tw.holidaybear.jetpack.exercise
+
+import com.google.common.truth.Truth.assertThat
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import retrofit2.Response
+
+@RunWith(JUnit4::class)
+class GitHubAPITest {
+
+    @MockK
+    lateinit var api: GitHubAPI
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun testUsersResponse() {
+        runBlocking {
+            coEvery { api.getUsers(any()) } returns Response.success(testUsers)
+
+            val apiResponse = api.getUsers("token")
+
+            assertThat(apiResponse.isSuccessful)
+            assertThat(apiResponse.body() != null)
+            assertThat(apiResponse.body()?.size == 2)
+        }
+    }
+
+    @Test
+    fun testUserResponse() {
+        runBlocking {
+            coEvery { api.getUser(any(), any()) } returns Response.success(testUser)
+
+            val apiResponse = api.getUser("token", "login")
+
+            assertThat(apiResponse.isSuccessful)
+            assertThat(apiResponse.body() != null)
+            assertThat(apiResponse.body()?.login == "JakeWharton")
+        }
+    }
+}
---
+++ app/src/test/java/tw/holidaybear/jetpack/exercise/TestData.kt
@@ -0,0 +1,36 @@
+package tw.holidaybear.jetpack.exercise
+
+val testUsers = listOf(
+    User(
+    66577,
+    "JakeWharton",
+    "https://avatars0.githubusercontent.com/u/66577?v=4",
+    false,
+    "Jake Wharton",
+    "http://jakewharton.com",
+    "Pittsburgh, PA, USA",
+    null
+    ),
+    User(
+    89202,
+    "yigit",
+    "https://avatars0.githubusercontent.com/u/89202?v=4",
+    false,
+    "Yigit Boyar",
+    "birbit.com",
+    "san francisco",
+    null
+    )
+)
+
+val testUser =
+    User(
+    66577,
+    "JakeWharton",
+    "https://avatars0.githubusercontent.com/u/66577?v=4",
+    false,
+    "Jake Wharton",
+    "http://jakewharton.com",
+    "Pittsburgh, PA, USA",
+    null
+    )
