d9c894c431a96127648cf568c041d4447e2adce1
Kami <yichen@mozilla.com>
2019-07-15 14:40:00 +0800

    Implement API model

---
+++ app/src/main/java/tw/holidaybear/jetpack/exercise/GitHubAPI.kt
@@ -0,0 +1,27 @@
+package tw.holidaybear.jetpack.exercise
+
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+
+interface GitHubAPI {
+
+    @GET("/users")
+    suspend fun getUsers(@Header("Authorization") authorization: String): Response<List<User>>
+
+    @GET("/users/{login}")
+    suspend fun getUser(@Header("Authorization") authorization: String, @Path("login") login: String): Response<User>
+
+    companion object {
+        fun create(): GitHubAPI {
+            return Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GitHubAPI::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/tw/holidaybear/jetpack/exercise/User.kt
@@ -0,0 +1,14 @@
+package tw.holidaybear.jetpack.exercise
+
+import com.google.gson.annotations.SerializedName
+
+data class User(
+    @SerializedName("id") val id: Long,
+    @SerializedName("login") val login: String,
+    @SerializedName("avatar_url") val imageUrl: String,
+    @SerializedName("site_admin") val isAdmin: Boolean,
+    @SerializedName("name") val name: String?,
+    @SerializedName("blog") val blog: String?,
+    @SerializedName("location") val location: String?,
+    @SerializedName("bio") val bio: String?
+)
